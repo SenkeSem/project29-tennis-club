33a9f7e62160746a0c286dff0df97fa93e4da325
SenkeSem <sen9morf@gmail.com>
2024-03-18 17:03:43 +0300

    Добавляет блок с услугами

--- src/components/ServicesBlock/ServicesBlock.jsx
+++ src/components/ServicesBlock/ServicesBlock.jsx
@@ -3,7 +3,22 @@ import styles from './ServicesBlock.module.scss';
 const ServicesBlock = () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Блок с информацией об услугах</h1>
+      <section className={styles.header}>
+        <h3>Услуги</h3>
+        <article>
+          <button>
+            <img src="/left.svg" alt="left" />
+          </button>
+          <button>
+            <img src="/right.svg" alt="right" />
+          </button>
+        </article>
+      </section>
+      <section className={styles.serviceImg}>
+        <div className={styles.photo}>
+          <p>Уютное кафе</p>
+        </div>
+      </section>
     </div>
   );
 };
